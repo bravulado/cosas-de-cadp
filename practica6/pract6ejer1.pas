585cf9cb38b2c54f81f864df76f9f6cd4003f536
bravulado <matyasgaleano05@gmail.com>
2024-10-16 17:43:10 -0300

    Update pract6ejer1.pas

--- practica6/pract6ejer1.pas
+++ practica6/pract6ejer1.pas
@@ -1,42 +1,43 @@
 program JugamosConListas;
 type
-lista = ^nodo;
-nodo = record
-num : integer;
-sig : lista;
-end;
+	lista = ^nodo;
+	nodo = record
+	    num : integer;
+	     sig : lista;
+	end;
+ 
 procedure armarNodo(var L: lista; v: integer);
 var
-aux : lista;
+	aux : lista;
 begin
-new(aux);
-aux^.num := v;
-aux^.sig := L;
-L := aux;
+   new(aux);
+   aux^.num := v;
+   aux^.sig := L;
+   L := aux;
 end;
-
 
 procedure imprimir (pri:lista);
 begin
-	while(pri <> nil)do
-	begin
-		writeln('dsfsdf',pri^.num);
-		pri:=pri^.sig;
-	end;
+   while(pri <> nil)do
+   begin
+      writeln('dsfsdf',pri^.num);
+       pri:=pri^.sig;
+    end;
 end;
 
 
 var
-pri : lista;
-valor : integer;
+  pri : lista;
+  valor : integer;
 begin
-pri := nil;
-writeln('Ingrese un numero');
-read(valor);
-while (valor <> 0) do begin
-armarNodo(pri, valor);
-writeln('ingrese un numero');
-read(valor);
-end;
-imprimir(pri);
+  pri := nil;
+  writeln('Ingrese un numero');
+   read(valor);
+   while (valor <> 0) do 
+   begin
+     armarNodo(pri, valor);
+     writeln('ingrese un numero');
+     read(valor);
+   end;
+   imprimir(pri);
 end.
